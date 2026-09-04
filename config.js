44f5a63c1f6d01bb3febb50125a3e8b2d3b8452a
parrot <parrot@localhost.localdomain>
2026-09-04 11:15:36 -0700

    made config.js for website and password management

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+const TEACHER_PASSWORD = "CHSteacher";
+const GOOGLE_DRIVE_FOLDER_URL = "https://docs.google.com/spreadsheets/d/1u4bldXEIZOZfQDXL4WdcKkSm_OgID72YxU8jFkj24Rw/edit?usp=sharing";
